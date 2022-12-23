61befcf27933dfe985458e7662f46ec941a5b663
Akshay <akshaynighot73@gmail.com>
2022-12-23 22:26:37 +0530

    deleted notes fragment implemented

--- app/src/main/java/com/android/note/keeper/ui/deleted/DeletedNoteFragment.kt
+++ app/src/main/java/com/android/note/keeper/ui/deleted/DeletedNoteFragment.kt
@@ -1,32 +1,58 @@
 package com.android.note.keeper.ui.deleted
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuProvider
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import com.android.note.keeper.R
+import com.android.note.keeper.data.PreferenceManager
+import com.android.note.keeper.databinding.FragmentArchiveNoteBinding
+import com.android.note.keeper.databinding.FragmentDeletedNoteBinding
+import com.android.note.keeper.ui.MainActivity
+import kotlinx.coroutines.launch
 
 
-class DeletedNoteFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            //todo
-        }
-    }
+class DeletedNoteFragment : Fragment(R.layout.fragment_deleted_note) , MenuProvider {
+    private var _binding: FragmentDeletedNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deleted_note, container, false)
+        _binding = FragmentDeletedNoteBinding.inflate(inflater, container, false)
+
+        // DemoUtils.addBottomSpaceInsetsIfNeeded(binding.root as ViewGroup, container)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentArchiveNoteBinding.bind(view)
+        (activity as MainActivity).readMode.isVisible = false
 
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_deleted, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.action_empty_bin -> {
+                //todo delete all notes
+                true
+            }
+            else -> false
+        }
     }
 }
