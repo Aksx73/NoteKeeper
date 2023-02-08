a13328633d5dda4dd3446579a7c4981d73d85299
Akshay <akshaynighot73@gmail.com>
2023-02-08 22:26:28 +0530

    about page ui tweaks

--- app/src/main/java/com/android/note/keeper/data/local/NoteDao.kt
+++ app/src/main/java/com/android/note/keeper/data/local/NoteDao.kt
@@ -1,11 +1,7 @@
 package com.android.note.keeper.data.local
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
+
 import com.android.note.keeper.data.model.Note
 import com.android.note.keeper.util.Constants
 import kotlinx.coroutines.flow.Flow
@@ -19,10 +15,10 @@ interface NoteDao {
     @Query("SELECT * FROM notes_table WHERE archived = 1 AND ((title LIKE '%' || :searchQuery || '%') OR (content LIKE '%' || :searchQuery || '%')) ORDER BY created DESC") //ORDER BY pin DESC, created DESC
     fun getAllArchiveNotes(searchQuery: String): Flow<List<Note>>
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAndGetID(note: Note): Long
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(note: Note)
 
     @Query("SELECT * FROM notes_table WHERE _id=:id")
